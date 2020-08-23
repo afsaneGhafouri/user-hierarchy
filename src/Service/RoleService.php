f2afe4b98092e2f2368e20c4e0c3eadd36db84e8
afsane ghafoori <afsane.ghafoori@metigy.com>
2020-08-23 15:35:19 +1000

    Create RoleService

---
+++ src/Service/RoleService.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Service;
+
+use App\Entity\User;
+
+class RoleService
+{
+    private array $roles;
+    private array $users;
+
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+
+    public function setUsers(array $users): void
+    {
+        $this->users = $users;
+    }
+
+    public function getSubOrdinates(User $givenUser): array
+    {
+        $givenUserLevel = $givenUser->getRole()->getLevel();
+        $result = [];
+        /* @var $user User */
+        foreach ($this->users as $user) {
+            if ($user->getRole()->getLevel() > $givenUserLevel) {
+                $result[] = $user;
+            }
+        }
+
+        return $result;
+    }
+
+}
